2a7dc7adb1d6a6bf38bc6fbcee3f7c0dfc8cc322
maozhijian <chiefmaster@yeah.net>
2024-12-22 16:04:14 +0800

    save

--- config/biz.php
+++ config/biz.php
@@ -2,5 +2,5 @@
 return [
     'topOfTheMoonTokens' =>
         explode(',',env('BIZ_TOP_OF_THE_MOON_TOKEN', '0xbe1bcaa424ce1a52c41a68b4804c59ad17036104')),
-    'fundingGoalMetis'=>'200000000000000000',
+    'fundingGoalMetis'=> env('FUNDING_GOAL_METIS','20000000000000000000'),
 ];
