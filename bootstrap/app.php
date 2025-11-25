80b0e7af437e242e7e4967dd61bda3ae9962de03
Jim0xff <mzj@p1xlabs.com>
2025-11-25 18:19:46 +0800

    save

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
         $middleware->append(\App\Http\Middleware\GlobalThrottleRequestsMiddleware::class);
         $middleware->append(\App\Http\Middleware\AssignRequestId::class);
         $middleware->append(\App\Http\Middleware\LogRequest::class);
-        $middleware->append(\App\Http\Middleware\VerifySign::class);
+        //$middleware->append(\App\Http\Middleware\VerifySign::class);
         $middleware->validateCsrfTokens(except: [
             'stripe/*',
             '/*',
